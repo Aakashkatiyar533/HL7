bc454f76bdd1ec57a13f54d47afd00502f1da9ce
Aakashkatiyar533 <Aakashkatiyar533@gmail.com>
2019-07-23 19:22:33 +0530

    Update randomValues.js

--- JS/randomValues.js
+++ JS/randomValues.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
 
 //console.log($('input[name="gender"]:checked').val());
-var index = msh.indexOf("AllergenCode");
+var index = adtArray.indexOf("asdddd");
 console.log(index);
 var arr = [];
 var zip = new JSZip();
@@ -23,7 +23,6 @@ mm.length == 1 && (mm = "0" + mm);
 hh.length == 1 && (hh = "0" + hh);
 mins.length == 1 && (mins = "0" + mins);
 ss.length == 1 && (ss = "0" + ss);
-var yyyymmdd = yyyy + mm + dd + hh + mm + ss;
 
 $("#save-btn").click(function () {
   var patient_class = "";
@@ -43,45 +42,46 @@ $("#save-btn").click(function () {
   });
   gender = gender.substring(0, gender.length - 1);
   if (gender == "Male")
-    msh.splice(32, 1, "M");
+    adtArray.splice(32, 1, "M");
   if (gender == "Female")
-    msh.splice(32, 1, "F");
+    adtArray.splice(32, 1, "F");
   if (gender == "Unknown")
-    msh.splice(32, 1, "U");
+    adtArray.splice(32, 1, "U");
   var incr = $('#messagecount').val();
   var message_type = $('#messagetype').val();
-  msh.splice(8, 1, "ADT^" + message_type);
-  msh.splice(20, 1, "ADT^" + message_type);
+  adtArray.splice(8, 1, "ADT^" + message_type);
+  adtArray.splice(20, 1, message_type);
   //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
   for (var i = 0; i < incr; i++) { //2035
     if (message_type == "Random") {
       var random_message_type = messageTypeArray[(Math.random() * messageTypeArray.length) | 0];
-      msh.splice(8, 1, "ADT^" + random_message_type);
-      msh.splice(20, 1, "ADT^" + random_message_type);
+      adtArray.splice(8, 1, "ADT^" + random_message_type);
+      adtArray.splice(20, 1, "ADT^" + random_message_type);
     }
+    var yyyymmdd = yyyy + mm + dd + hh + mm + ss;
     //MSH7
-    msh.splice(6, 1, yyyymmdd);
+    adtArray.splice(6, 1, yyyymmdd);
     //EVN.2
-    msh.splice(21, 1, yyyymmdd);
+    adtArray.splice(21, 1, yyyymmdd);
     //Random Patitient ID
     var pid = Math.floor(Math.random() * (100000 - 1000 + 1)) + 1000;
-    msh.splice(9, 1, pid + "M2210")
-    msh.splice(26, 1, pid);
-    msh.splice(27, 1, pid);
-    msh.splice(55, 1, pid);
+    adtArray.splice(9, 1, pid + "M2210")
+    adtArray.splice(26, 1, pid);
+    adtArray.splice(27, 1, pid);
+    adtArray.splice(55, 1, pid);
     // Random Patient Name
     var firstrand = firstname[(Math.random() * firstname.length) | 0];
     var lastrand = lastname[(Math.random() * lastname.length) | 0];
     var midrand = middlename[(Math.random() * lastname.length) | 0];
-    msh.splice(29, 1, lastrand + "^" + firstrand + "^" + midrand);
+    adtArray.splice(29, 1, lastrand + "^" + firstrand + "^" + midrand);
     //address
     var address = addr[(Math.random() * addr.length) | 0];
-    msh.splice(35, 1, address);
+    adtArray.splice(35, 1, address);
     //phone number
     var areacode = Math.floor(Math.random() * (999 - 100 + 1) + 100);
     var prefix = Math.floor(Math.random() * (999 - 100 + 1) + 100);
     var line_number = Math.floor(1000 + Math.random() * 9000);
-    msh.splice(37, 1, "(" + areacode + ") " + prefix + "-" + line_number);
+    adtArray.splice(37, 1, "(" + areacode + ") " + prefix + "-" + line_number);
     //Patient Class
     if (jQuery.inArray("Random", patientClassArray) == -1) {
       var patientClass1 = patientClassArray[(Math.random() * patientClassArray.length) | 0];
@@ -93,38 +93,60 @@ $("#save-btn").click(function () {
     //PID.8 If gender = random
     if (gender == "Random") {
       var random_gender = genderarray[Math.floor(Math.random() * genderarray.length)];
-      msh.splice(32, 1, random_gender);
+      adtArray.splice(32, 1, random_gender);
     }
-    msh.splice(76, 1, patientClass1);
-    msh.splice(60, 1, patientClass2);
+    adtArray.splice(76, 1, patientClass1);
+    adtArray.splice(60, 1, patientClass2);
     //Visit ID
-    msh.splice(42, 1, pid + "VZ5097");
-    msh.splice(77, 1, pid + "VZ5097");
+    adtArray.splice(42, 1, pid + "VZ5097");
+    adtArray.splice(77, 1, pid + "VZ5097");
     //PV1.3
     var poc = points_of_care[(Math.random() * points_of_care.length) | 0];
     var rooms = Math.floor(Math.random() * (100 - 121 + 1)) + 121;
     var dept = department[(Math.random() * department.length) | 0];
-    msh.splice(61, 1, poc + "^" + rooms + "^" + "^" + "^" + "^" + "^" + "^" + "^" + dept);
+    adtArray.splice(61, 1, poc + "^" + rooms + "^" + "^" + "^" + "^" + "^" + "^" + "^" + dept);
     //PV1.7 
     var npi = Math.floor(Math.random() * (1000000000 - 100000000 + 1)) + 100000000;
     var provider_last_rand = lastname[(Math.random() * lastname.length) | 0];
     var provider_first_rand = middlename[(Math.random() * lastname.length) | 0];
-    msh.splice(65, 1, npi + "^" + provider_last_rand + "^" + provider_first_rand);
+    adtArray.splice(65, 1, npi + "^" + provider_last_rand + "^" + provider_first_rand);
     //PV1.17
     var npi = Math.floor(Math.random() * (1000000000 - 100000000 + 1)) + 100000000;
     var provider_last_rand = lastname[(Math.random() * lastname.length) | 0];
     var provider_first_rand = middlename[(Math.random() * lastname.length) | 0];
-    msh.splice(75, 1, npi + "^" + provider_last_rand + "^" + provider_first_rand);
+    adtArray.splice(75, 1, npi + "^" + provider_last_rand + "^" + provider_first_rand);
+    //PV1.44
+    adtArray.splice(102, 1, yyyymmdd);
     //AL1.3
     var allergyCode = allergenCode[(Math.random() * allergenCode.length) | 0];
-    msh.splice(101, 1, allergyCode);
+    adtArray.splice(106, 1, allergyCode);
+    //GT1.3
+    var guarantorFname = lastname[(Math.random() * lastname.length) | 0];
+    var guarantorLname = firstname[(Math.random() * firstname.length) | 0];
+    adtArray.splice(113, 1, guarantorFname + "^" + guarantorLname);
+    //GT1.5
+    adtArray.splice(115, 1, address);
+    //GT1.6
+    var gareacode = Math.floor(Math.random() * (999 - 100 + 1) + 100);
+    var gprefix = Math.floor(Math.random() * (999 - 100 + 1) + 100);
+    var gline_number = Math.floor(1000 + Math.random() * 9000);
+    adtArray.splice(116, 1, "(" + gareacode + ") " + gprefix + "-" + gline_number);
+    //GT1.11
+    var grelationship = relationship[(Math.random() * relationship.length) | 0];
+    adtArray.splice(121, 1, grelationship);
+    //IN1.2
+    var insuranceName = insurance[(Math.random() * insurance.length) | 0];
+    adtArray.splice(124, 1, insuranceName);
+    //IN1.3
+    var insuranceCompanyName = insuranceCompany[(Math.random() * insuranceCompany.length) | 0];
+    adtArray.splice(125, 1, insuranceCompanyName);
     /* for (var j=0;j<msh.length;j++)
     {document.write(msh[j] + "|");
     }
     document.write("<br><br><br><br>"); */
 
-    for (var j = 0; j < msh.length; j++) {
-      arr.push(msh[j].toString() + "|");
+    for (var j = 0; j < adtArray.length; j++) {
+      arr.push(adtArray[j].toString() + "|");
     }
     zip.add("ADT " + (i + 1) + ".txt", arr.join(''));
     arr = [];
